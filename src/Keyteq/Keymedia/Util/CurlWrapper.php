57b9fabdecd26ef4b309629834eaf40185ca18be
Jakub Zienkiewicz <jakub.zienkiewicz@espeo.pl>
2013-10-09 12:15:04 +0200

    Handle POST and PUT methods

--- src/Keyteq/Keymedia/Util/CurlWrapper.php
+++ src/Keyteq/Keymedia/Util/CurlWrapper.php
@@ -5,13 +5,20 @@ namespace Keyteq\Keymedia\Util;
 class CurlWrapper
 {
 
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+
     protected $ch;
     protected $url;
+    protected $method;
 
-    public function __construct($url = null)
+    public function __construct($url = null, $method = self::METHOD_GET)
     {
         $this->url = $url;
         $this->ch = curl_init($url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        $this->setMethod($method);
     }
 
     public function getUrl()
@@ -23,5 +30,29 @@ class CurlWrapper
     {
         $this->url = $url;
         curl_setopt($this->ch, CURLOPT_URL, $url);
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function setMethod($method)
+    {
+        switch ($method) {
+
+            case static::METHOD_GET:
+                curl_setopt($this->ch, CURLOPT_HTTPGET, true);
+                break;
+            case static::METHOD_POST:
+                curl_setopt($this->ch, CURLOPT_POST, true);
+                break;
+            case static::METHOD_PUT:
+                curl_setopt($this->ch, CURLOPT_PUT, true);
+                break;
+            default:
+                throw new \InvalidArgumentException("Method not supported: {$method}");
+        }
+        $this->method = $method;
     }
 }
--- tests/Keyteq/Keymedia/Util/CurlWrapperTest.php
+++ tests/Keyteq/Keymedia/Util/CurlWrapperTest.php
@@ -20,6 +20,13 @@ class CurlWrapperTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\Keyteq\Keymedia\Util\CurlWrapper', $cw);
     }
 
+    public function testConstructSetsMethodToGetByDefault()
+    {
+        $expected = CurlWrapper::METHOD_GET;
+        $actual = $this->ch->getMethod();
+        $this->assertEquals($expected, $actual);
+    }
+
     public function testGetUrl()
     {
         $expected = 'http://example2.com';
@@ -38,4 +45,31 @@ class CurlWrapperTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $actual);
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetMethodThrowsOnInvalidMethod()
+    {
+        $this->ch->setMethod('invalid method');
+    }
+
+    /**
+     * @dataProvider validMethods
+     * @param string $method HTTP method
+     */
+    public function testSetMethod($method)
+    {
+        $this->ch->setMethod($method);
+        $this->assertEquals($method, $this->ch->getMethod());
+    }
+
+
+    public function validMethods()
+    {
+        return array(
+            array('GET'),
+            array('POST'),
+            array('PUT')
+        );
+    }
 }
