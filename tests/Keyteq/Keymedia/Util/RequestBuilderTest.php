4c7d487604303f102053b145e6cf93652d5fe6b6
Jakub Zienkiewicz <jakub.zienkiewicz@espeo.pl>
2013-10-30 11:53:10 +0100

    Add tests for RequestBuilder

--- tests/Keyteq/Keymedia/Util/RequestBuilderTest.php
+++ tests/Keyteq/Keymedia/Util/RequestBuilderTest.php
@@ -3,15 +3,44 @@
 namespace Keyteq\Keymedia\Util;
 
 use Keyteq\Keymedia\BaseTest;
+use Keyteq\Keymedia\Util\RequestBuilder;
 
 class RequestBuilderTest extends BaseTest
 {
+
+    protected $builder;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->builder = new RequestBuilder($this->apiConfig);
+    }
+
     /**
      * @dataProvider requestDataProvider
      */
-    public function testBuildRequest($url, $method, $parameters)
+    public function testBuildRequestSetsProperUrl($url, $method, $parameters)
     {
-        $this->markTestIncomplete();
+        $request = $this->builder->buildRequest($url, $method, $parameters);
+        $this->assertContains($url, (array) $request);
+    }
+
+    /**
+     * @dataProvider requestDataProvider
+     */
+    public function testBuildRequestSetsProperMethod($url, $method, $parameters)
+    {
+        $request = $this->builder->buildRequest($url, $method, $parameters);
+        $this->assertContains($method, (array) $request);
+    }
+
+    /**
+     * @dataProvider requestDataProvider
+     */
+    public function testBuildRequestSetsParameters($url, $method, $parameters)
+    {
+        $request = $this->builder->buildRequest($url, $method, $parameters);
+        $this->assertContains($parameters, (array) $request);
     }
 
     public function requestDataProvider()
@@ -21,6 +50,19 @@ class RequestBuilderTest extends BaseTest
                 'http://host.name/path',
                 'GET',
                 array()
+            ), array(
+                'http://some.host/some-path',
+                'POST',
+                array(
+                    'k1' => 'v1'
+                )
+            ), array(
+                'http://some.other.host/some-other-path',
+                'PUT',
+                array(
+                    'q' => 'search',
+                    'tags' => 'album'
+                )
             )
         );
     }
